7e70c1ca4e53434d13aae1078f8c457d8e7c6052
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2021-01-31 17:54:12 +0100

    unused fn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    for i in 1..3 {
+        println!("Hello, world! {}", invert(i));
+
+    }
+}
+
+fn invert(i: i32) -> i32 {
+    return -i
+}
+
+
+fn invert2(i: f64) -> f64 {
+    return -i
 }
